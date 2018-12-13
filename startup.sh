1b56ad5d0b142d4f12c76f3dec984737bf12d5db
Paul Bisso <41169757+paulbisso@users.noreply.github.com>
2018-12-13 17:44:15 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -17,3 +17,4 @@ rm test.zip
 cd /home/ubuntu/src/derm-ai
 git clone https://github.com/paulbisso/ISIC2017_with_optimization
 conda env create -f /home/ubuntu/src/derm-ai/ISIC2017_with_optimization/dermAIenv.yml
+echo ". /home/ubuntu/anaconda3/etc/profile.d/conda.sh" >> ~/.bashrc
