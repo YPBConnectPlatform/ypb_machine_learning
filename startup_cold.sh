8c5b490232254f2deb9ebca7a0e8a7474ef8acca
Paul Bisso <41169757+paulbisso@users.noreply.github.com>
2019-03-01 05:57:51 -0500

    Combine startup_cold and startup_cool

--- startup_cold.sh
+++ startup_cold.sh
@@ -3,3 +3,8 @@ cd src
 mkdir derm-ai
 cd derm-ai
 git clone https://github.com/udacity/dermatologist-ai.git
+mkdir -p /home/ubuntu/src/derm-ai
+cd /home/ubuntu/src/derm-ai
+git clone https://github.com/paulbisso/ISIC2017_with_optimization
+conda env create -f /home/ubuntu/src/derm-ai/ISIC2017_with_optimization/dermAIenv.yml
+echo ". /home/ubuntu/anaconda3/etc/profile.d/conda.sh" >> ~/.bashrc
