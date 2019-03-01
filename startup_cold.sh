a8a18ea5b2072271c570c8b2302f4f68c8c34137
Paul Bisso <41169757+paulbisso@users.noreply.github.com>
2019-03-01 05:57:10 -0500

    Eliminate data copying steps
    Data now stored in S3, instructions in readme

--- startup_cold.sh
+++ startup_cold.sh
@@ -3,14 +3,3 @@ cd src
 mkdir derm-ai
 cd derm-ai
 git clone https://github.com/udacity/dermatologist-ai.git
-mkdir data
-cd data
-wget https://s3-us-west-1.amazonaws.com/udacity-dlnfd/datasets/skin-cancer/train.zip
-unzip train.zip
-rm train.zip
-wget https://s3-us-west-1.amazonaws.com/udacity-dlnfd/datasets/skin-cancer/valid.zip
-unzip valid.zip
-rm valid.zip
-wget https://s3-us-west-1.amazonaws.com/udacity-dlnfd/datasets/skin-cancer/test.zip
-unzip test.zip
-rm test.zip
