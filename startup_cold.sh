1fe9d89aa1a76cfbd48aba3db991b894489c91b8
Paul Bisso <41169757+paulbisso@users.noreply.github.com>
2019-03-05 12:31:16 -0500

    Git clones multi-node-horovod, not master

--- startup_cold.sh
+++ startup_cold.sh
@@ -2,9 +2,9 @@
 cd src
 mkdir derm-ai
 cd derm-ai
-git clone -b multi-node-horovod https://github.com/udacity/dermatologist-ai.git
+git clone https://github.com/udacity/dermatologist-ai.git
 mkdir -p /home/ubuntu/src/derm-ai
 cd /home/ubuntu/src/derm-ai
-git clone https://github.com/paulbisso/ISIC2017_with_optimization
+git clone -b multi-node-horovod https://github.com/paulbisso/ISIC2017_with_optimization
 conda env create -f /home/ubuntu/src/derm-ai/ISIC2017_with_optimization/dermAIenv.yml
 echo ". /home/ubuntu/anaconda3/etc/profile.d/conda.sh" >> ~/.bashrc
